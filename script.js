f428e1b462022ee00029244a1470c6f41973cfd6
aaronlee09-max <aaronlee09@goedu.kr>
2026-09-21 22:55:54 +0900

    revert: restore pre-classification font UI script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-/* Fontory script is pinned below in index.html to the last known-good commit. */
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/aaronlee09-max/datatronics@3edd3ab2785134362d172da2772999e0b7d1bfc7/script.js";
+  script.defer = false;
+  document.currentScript.parentNode.insertBefore(script, document.currentScript.nextSibling);
+})();
